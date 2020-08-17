961424a6e3a7a698b1f309d1bafeefa1fb152dc0
Farjana Islam <nipa.farzana87@gmailcon>
2020-08-17 12:23:49 +0600

    updating code

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// //  https://api.lyrics.ovh/v1
+// //  https://api.lyrics.ovh/
 
 
 
@@ -14,26 +14,29 @@ document.getElementById('searchBtn').addEventListener('click', function(){
 
 function displaySongDetails(allData){
     let songData = allData.data;
-    // console.log(songData);
-    // console.log(songList);
-    // let songListArray = [];
-    // console.log(songListArray);
-    const songListDiv = document.getElementById('firstTenSong');
+
+    let songList = [];
+    const songListDisplay = document.getElementById('firstTenSong');
     for (let i = 0; i < songData.length; i++) {
         const item = songData[i];
+
         const songTitle = item.title;
         const artistName = item.artist.name;
-        const songDetails = `${songTitle} powered by ${artistName}`
-        // console.log(songDetails);
-        // songListArray.push(songDetails);
-        // let displaySongList = songListArray.slice(0, 10);
-        // document.getElementById('firstTenSong').innerText = displaySongList;
-        // const songListDiv = document.getElementById('firstTenSong');
-        const li = document.createElement('li');
-        li.innerText = songDetails;
-        songListDiv.appendChild(li);
-    }
-}
+        const songDetails = `Title: ${songTitle} Artist: ${artistName}`
+        songList.push(songDetails);
+        if (songList.length <= 10) {
+            const li = document.createElement('li');
+            li.innerText = songDetails;
+            songListDisplay.appendChild(li);
+        };
+    
+    };
+    document.getElementById('searchInput').addEventListener('click', function(){
+        songListDisplay.innerText = "";
+    })
+};
+
+
 
 
 
